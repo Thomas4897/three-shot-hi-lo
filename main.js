cadc3bbfa3a4797f61a12a21de45ab66701e6cbf
Thomas4897 <t.maynard4897@gmail.com>
2021-09-29 03:04:33 +0800

    Starting to add Code

---
+++ main.js
@@ -0,0 +1,36 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+console.log("I'm thinking of a number bewtween 1 and 100. Try to guess it.");
+let guess = Number(prompt("> "));
+console.log("");
+
+const random = Number(Math.ceil(Math.random() * 1));
+console.log(random);
+
+let count = 0;
+
+while (guess !== random) {
+	if (guess === random && count > 1) {
+		console.log("Sorry too low. I was thinking of " + random + ".");
+		break;
+	} else if (count > 2) {
+		break;
+	} else if (guess < random) {
+		console.log("Sorry, too low! Guess again.");
+		guess = prompt("> ");
+		console.log("");
+	} else if (guess > random) {
+		console.log("Sorry, too high! Guess again.");
+		guess = prompt("> ");
+		console.log("");
+	} else {
+		console.log("Congratulations, " + guess + " is correct!");
+	}
+
+	count = count + 1;
+}
+if (guess === random) {
+	console.log("Congratulations, " + guess + " is correct!");
+} else {
+	console.log("");
+}
